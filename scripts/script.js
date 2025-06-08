83b0b586507d74e29ff6fb90191c9ce13d622d61
lena234141 <lena.izyukova05@mail.ru>
2025-06-08 14:01:40 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-file:///C:/Users/Root/Desktop/proect%203.1/scripts/script.js
+<html><head><style>
+        pre {
+          content-visibility: auto;
+          line-height: 1.2em;
+          margin: 0;
+        }
+    </style>
+    <meta name="\&quot;color-scheme\&quot;" content="\&quot;light" dark\"=""></head><body><pre style="width: 69ch; contain-intrinsic-height: 6em; content-visibility: visible">'use strict'
+
+document.addEventListener("DOMContentLoaded", () =&gt; {
+    console.log('Скрипт отработал корректно')
+});
+</pre>
+</body></html>
